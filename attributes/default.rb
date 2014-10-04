95086623f00dee8567149cbdb8e8dc3729c0c4ad
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-10-04 18:38:54 +0200

    Rename some attribute related to the application

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,14 +9,14 @@ default['php']['secure_functions'] = false
 default['php']['ini_settings']['memory_limit'] = '512M'
 default['php']['ini_settings']['date.timezone'] = 'Europe/Berlin'
 
-default['site-builddocstypo3org']['app']['owner'] = "docsbuilder"
+default['site-builddocstypo3org']['app']['owner'] = "builddocstypo3org"
 default['site-builddocstypo3org']['app']['home'] = "/var/www/vhosts/build.docs.typo3.org"
 default['site-builddocstypo3org']['app']['server_name'] = "build.docs.typo3.org"
 default['site-builddocstypo3org']['app']['server_alias'] = nil
 #default['site-builddocstypo3org']['app']['fqdn'] = "build.docs.typo3.org"
 default['site-builddocstypo3org']['app']['context'] = "Production"
 
-default['site-builddocstypo3org']['database']['name'] = "docsbuilder"
-default['site-builddocstypo3org']['database']['username'] = "docsbuilder"
+default['site-builddocstypo3org']['database']['name'] = "builddocstypo3org"
+default['site-builddocstypo3org']['database']['username'] = "builddocs"
 default['site-builddocstypo3org']['database']['password'] = nil
 default['site-builddocstypo3org']['database']['hostname'] = "localhost"
